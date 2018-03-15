4336144dcfd08bf33f1ac3c9696bb8e9efc8e559
viagram <viagram@users.noreply.github.com>
2018-03-15 18:38:55 +0800

    fix

--- bbr.sh
+++ bbr.sh
@@ -33,49 +33,31 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function printnew(){
     typeset -l CHK
     WENZHI=""
-    RIGHT=0
+    COLOUR=""
     HUANHANG=0
     for PARSTR in "${@}";do
         CHK="${PARSTR}"
         if echo "${CHK}" | egrep -io "^\-[[:graph:]]*" >/dev/null 2>&1; then
-            if [[ "${CHK}" == "-black" ]]; then
-                COLOUR="\033[30m"
-            elif [[ "${CHK}" == "-red" ]]; then
-                COLOUR="\033[31m"
-            elif [[ "${CHK}" == "-green" ]]; then
-                COLOUR="\033[32m"
-            elif [[ "${CHK}" == "-yellow" ]]; then
-                COLOUR="\033[33m"
-            elif [[ "${CHK}" == "-blue" ]]; then
-                COLOUR="\033[34m"
-            elif [[ "${CHK}" == "-purple" ]]; then
-                COLOUR="\033[35m"
-            elif [[ "${CHK}" == "-cyan" ]]; then
-                COLOUR="\033[36m"
-            elif [[ "${CHK}" == "-white" ]]; then
-                COLOUR="\033[37m"
-            elif [[ "${CHK}" == "-a" ]]; then
-                HUANHANG=1
-            elif [[ "${CHK}" == "-r" ]]; then
-                RIGHT=1
-            fi
+            case "${CHK}" in
+                -black) COLOUR="\033[30m";;
+                -red) COLOUR="\033[31m";;
+                -green) COLOUR="\033[32m";;
+                -yellow) COLOUR="\033[33m";;
+                -blue) COLOUR="\033[34m";;
+                -purple) COLOUR="\033[35m";;
+                -cyan) COLOUR="\033[36m";;
+                -white) COLOUR="\033[37m";;
+                -a) HUANHANG=1 ;;
+                *) COLOUR="\033[37m";;
+            esac
         else
             WENZHI+="${PARSTR}"
         fi
     done
-    COUNT=$(echo -n "${WENZHI}" | wc -L)
-    if [[ ${RIGHT} -eq 1 ]];then
-        tput cup $(tput lines) $[$(tput cols)-${COUNT}]
-        printf "${COLOUR}%b%-${COUNT}s\033[0m" "${WENZHI}"
-        tput cup $(tput lines) 0
+    if [[ ${HUANHANG} -eq 1 ]];then
+        printf "${COLOUR}%b%s \033[0m" "${WENZHI}"
     else
-        tput cup $(tput lines) 0
-        if [[ ${HUANHANG} -eq 1 ]];then
-            printf "${COLOUR}%b%-${COUNT}s\033[0m" "${WENZHI}"
-            tput cup $(tput lines) ${COUNT}
-        else
-            printf "${COLOUR}%b%-${COUNT}s\033[0m\n" "${WENZHI}"
-        fi
+        printf "${COLOUR}%b%s\033[0m\n" "${WENZHI}"
     fi
 }
 
