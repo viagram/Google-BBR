166e590110ad76d35e19a4b6c2dd7e202269730e
viagram <viagram@users.noreply.github.com>
2019-09-20 20:44:48 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -282,8 +282,8 @@ function update_kernel(){
 			fi
 			grub_cfg="";
 			[[ -f /boot/grub2/grub.cfg ]] && grub_cfg=/boot/grub2/grub.cfg
-			[[ -f /boot/efi/EFI/centos/grub.cfg ]] && grub_cfg=/boot/efi/EFI/centos/grub.cfg
-			[[ -z {grub_cfg} ]] && (printnew -red "没找linu启动文件.";exit 1)
+			[[ -z ${grub_cfg} ]] && [[ -f /boot/efi/EFI/centos/grub.cfg ]] && grub_cfg=/boot/efi/EFI/centos/grub.cfg
+			[[ -z ${grub_cfg} ]] && (printnew -red "没找linu启动文件.";exit 1)
 			grub2-mkconfig -o ${grub_cfg} >/dev/null 2>&1
 			kernel_list=$(cat ${grub_cfg} | egrep -io "CentOS Linux[[:print:]]*\(core\)")
 			kernel_ver=$(echo "${kernel_list}" | egrep -io '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}' | sort -Vur | head -n1)
